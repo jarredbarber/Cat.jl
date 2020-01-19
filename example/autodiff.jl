7779df19335e56cd9f4d17c7bc831b352b658a77
Jarred Barber <jpb5082@gmail.com>
2020-01-19 13:49:53 -0500

    more complete autodiff

--- example/autodiff.jl
+++ example/autodiff.jl
@@ -1,13 +1,14 @@
 using Cat
 
-
-@category Smooth
-
-# The only objects in smooth are Nothing, R, and products of these
+# "Real" numbers
 const R = Float64
 
+# The only objects in Smooth are Nothing, R, and products of these
+@category Smooth where objects = obj -> obj in (Nothing, R, Tuple)
+# @category Linear ⊂ Smooth arrows = (Plus, Mult, Neg)
+
 # Basic algebra
-@arrow Smooth Plus :: (R, R) --> R
+@arrow Smooth Plus{T} :: (T, T) --> T
 @arrow Smooth Mult :: (R, R) --> R
 @arrow Smooth Neg :: R --> R
 # Calc 1
@@ -15,6 +16,7 @@ const R = Float64
 @arrow Smooth Sin :: R --> R
 @arrow Smooth Cos :: R --> R
 
+# Just an unbound input is equivalent to the identity morphism
 Variable = Smooth.Identity{R}
 
 Base.:+(a::Smooth.Arrow, b) = Plus(a, b)
@@ -50,10 +52,38 @@ end
     exp(a)
 end
 @interpret function (e::Eval)(m::Sin, a)
-    cos(a)
+    sin(a)
 end
 @interpret function (e::Eval)(m::Cos, a)
-    sin(a)
+    cos(a)
+end
+
+@functor Diff :: Smooth --> Smooth begin
+    objects = o -> (o, o)
+end
+
+@funct function (e::Diff)(m::Plus, (a, da), (b, db))
+    a+b, da + db
+end
+
+@funct function (e::Diff)(m::Mult, (a, da), (b, db))
+    a*b, a*da + b*db
+end
+
+@funct function (d::Diff)(m::Smooth.Constant, _)
+    m.value, zero()
+end
+
+@funct function (d::Diff)(m::Exp, (a, da))
+    Exp(a), Mult(da, Exp(a))
+end
+
+@funct function (e::Diff)(m::Sin, (a, da))
+    Sin(a), da*Cos(a),
+end
+
+@funct function (e::Diff)(m::Cos, (a, da))
+    Cos(a), -da*Sin(a)
 end
 
 # Test it out
@@ -61,4 +91,7 @@ x = Variable()
 y = cos(x + 3.0)
 z = sin(y) + cos(x)
 
+dz = Diff(z)
+
 println(Eval()(z, 1.0))
+println(Eval()(dz, (1.0, 1.0)))
